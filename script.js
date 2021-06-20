b794f352738c403f6f5509dba21d875d78dc1123
Alberinea <kan9870@gmail.com>
2021-06-20 22:27:23 +0900

    Added display book function

--- script.js
+++ script.js
@@ -12,18 +12,34 @@ function Book(title, author, pageNum, read) {
     this.read = read;
 }
 
+function displayBook() {
+    for (let index = 0; index < Library.length; index++) {
+        const element = Library[index];
+        const htmlText = `<h1>${element.title}</h1>
+                <p class="text">By ${element.author}</p>
+                <p class="text">${element.pageNum} pages</p>
+                <p class="text">${element.read}</p>`;
+        document.getElementById(index).innerHTML = htmlText;
+    }
+}
+
 function addBookToLibrary(e) {
     let titleVar = document.getElementById('title').value;
     let authorVar = document.getElementById('author').value;
     let pageNumVar = document.getElementById('pageNum').value;
     let readVar = document.getElementById('haveRead').value;
+    readVar = readVar === 'Yes' ? 'Read already' : 'Not read yet';
     if (form.checkValidity()) {
         const newBook = new Book(titleVar, authorVar, pageNumVar, readVar);
+        const newDiv = document.createElement('div');
+        document.getElementById('container').appendChild(newDiv);
+        newDiv.setAttribute('class', 'book');
+        newDiv.setAttribute('id', Library.length);
         Library.push(newBook);
         form.style.cssText = 'opacity: 0; pointer-events: none;';
         form.reset();
-        e.preventDefault()
-        console.log(Library);
+        displayBook();
+        e.preventDefault();
     }
 }
 
@@ -31,4 +47,3 @@ addBook.addEventListener('click', () => (form.style.cssText = 'opacity: 100'));
 closeButton.addEventListener('click', () => (form.style.cssText = 'opacity: 0; pointer-events: none;'));
 confirmButton.addEventListener('click', addBookToLibrary);
 
-console.log(Library);
